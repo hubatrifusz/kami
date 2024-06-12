92a9179930969f9b344d30f4070bd5f72f9825f5
Trifusz Huba <huba.trifusz@gmail.com>
2024-06-12 22:20:34 +0200

    picture_block + text_block + line DONE

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function setImageSize() {
+    let ImageBlocks = document.querySelectorAll('.picture_block');
+    let numberOfIMages = 0;
+    ImageBlocks.forEach(imageBlock => {
+       for (image of imageBlock.children) {
+        numberOfIMages += 1;
+       }
+       for (image of imageBlock.children) {
+        let imageWidth = `${(100/numberOfIMages)-10}%`
+        image.style.width = imageWidth;
+       }
+       numberOfIMages = 0;
+    });
+
+}
