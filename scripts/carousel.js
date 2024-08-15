b2b561c4ca32c9fc81d42624fe884340bc62baa2
[Trifusz <[trifusz.huba@students.jedlik.eu]>
2024-08-15 15:32:44 +0200

    scrolling done, spam clicking solved

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -9,10 +9,6 @@ let currentImageIndex = 2;
 
 imageArray = Array.from(imageContainer.children);
 
-carousel.style.scrollBehavior = 'auto';
-carousel.scrollLeft += (imageArray[currentImageIndex].getBoundingClientRect().left - window.innerWidth/2 + imageArray[currentImageIndex+1].getBoundingClientRect().width/2);
-carousel.style.scrollBehavior = 'smooth';
-
 const lastTwoClones = imageArray.slice(-2).map(img => img.cloneNode(true));
 const firstTwoClones = imageArray.slice(0,2).map(img => img.cloneNode(true));
 
@@ -22,30 +18,36 @@ lastTwoClones.forEach(img => imageContainer.appendChild(img));
 imageArray.forEach(img => imageContainer.appendChild(img));
 firstTwoClones.forEach(img => imageContainer.appendChild(img));
 
+carousel.scrollLeft = 0;
+carousel.style.scrollBehavior = 'auto';
+carousel.scrollLeft = (imageContainer.children[currentImageIndex].getBoundingClientRect().left - window.innerWidth/2 + imageContainer.children[currentImageIndex].getBoundingClientRect().width/2);
+carousel.style.scrollBehavior = 'smooth';
 
+console.log(imageContainer.children)
 //FUNCTIONS
 let isScrolling = false;
 
 function carouselScrollLeft() {
     if (isScrolling) return;
-
     isScrolling = true
 
-    carousel.scrollLeft -= (imageContainer.children[currentImageIndex].getBoundingClientRect().width/2) + (imageContainer.children[currentImageIndex-1].getBoundingClientRect().width/2);
+    carousel.scrollLeft = imageContainer.children[currentImageIndex-1].offsetLeft + imageContainer.children[currentImageIndex-1].clientWidth/2 - window.innerWidth/2;
     currentImageIndex--;
 
     if (currentImageIndex-1 == 0) {
+
         carousel.style.scrollBehavior = 'auto';
-        carousel.scrollLeft += imageContainer.children[imageContainer.children.length-2].getBoundingClientRect().left - window.innerWidth/2 + imageContainer.children[imageContainer.children.length-2].getBoundingClientRect().width/2;
+        carousel.scrollLeft = imageContainer.children[imageContainer.children.length-2].offsetLeft - window.innerWidth/2 + imageContainer.children[imageContainer.children.length-2].clientWidth/2;
         carousel.style.scrollBehavior = 'smooth';
+
         currentImageIndex = imageContainer.children.length-2;
-        carousel.scrollLeft -= (imageContainer.children[currentImageIndex].getBoundingClientRect().width/2) + (imageContainer.children[currentImageIndex-1].getBoundingClientRect().width/2);
+        carousel.scrollLeft = imageContainer.children[currentImageIndex-1].offsetLeft + imageContainer.children[currentImageIndex-1].clientWidth/2 - window.innerWidth/2;
         currentImageIndex--;
     }
 
     setTimeout(() => {
         isScrolling = false;
-    }, 300);
+    }, 500);
 
 
 }
@@ -54,19 +56,27 @@ function carouselScrollRight() {
     if (isScrolling) return;
     isScrolling = true
 
-    carousel.scrollLeft += (imageContainer.children[currentImageIndex].getBoundingClientRect().width/2) + (imageContainer.children[currentImageIndex+1].getBoundingClientRect().width/2);
+    carousel.scrollLeft = imageContainer.children[currentImageIndex+1].offsetLeft + imageContainer.children[currentImageIndex+1].clientWidth/2 - window.innerWidth/2;
     currentImageIndex++;
 
     if (currentImageIndex+1 == imageContainer.children.length-1) {
+
         carousel.style.scrollBehavior = 'auto';
-        carousel.scrollLeft += imageContainer.children[1].getBoundingClientRect().left - window.innerWidth/2 + imageContainer.children[1].getBoundingClientRect().width/2;
+        carousel.scrollLeft = imageContainer.children[1].offsetLeft - window.innerWidth/2 + imageContainer.children[1].clientWidth/2;
         carousel.style.scrollBehavior = 'smooth';
+
         currentImageIndex = 1
-        carousel.scrollLeft += (imageContainer.children[currentImageIndex].getBoundingClientRect().width/2) + (imageContainer.children[currentImageIndex+1].getBoundingClientRect().width/2);
+        carousel.scrollLeft = imageContainer.children[currentImageIndex+1].offsetLeft + imageContainer.children[currentImageIndex+1].clientWidth/2 - window.innerWidth/2;
         currentImageIndex++;
     }
 
     setTimeout(() => {
         isScrolling = false;
-    }, 300);
+    }, 500);
+}
+
+setInterval(carouselScrollRight, 5000);
+
+function logCarouselScrollLeft() {
+    console.log(imageContainer.children[currentImageIndex+1].offsetLeft)
 }
